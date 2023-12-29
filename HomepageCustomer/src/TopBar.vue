12a32e69a0b7f683099ddcae355d0cbcd103d209
Misakichan2k2 <147908643+Misakichan2k2@users.noreply.github.com>
2023-12-29 09:00:02 +0700

    username commit

--- HomepageCustomer/src/TopBar.vue
+++ HomepageCustomer/src/TopBar.vue
@@ -1,64 +1,156 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const showTooltip = ref(false);
+
+const hideTooltip = () => {
+  showTooltip.value = false;
+};
+
+const cancelHideTooltip = () => {
+  // Cancel the hide when the mouse enters the tooltip
+  showTooltip.value = true;
+};
+
+const menuUser = ref([
+  {
+    label: "Thông tin tài khoản",
+  },
+  {
+    label: "Lịch khám",
+  },
+  {
+    label: "Đánh giá",
+  },
+  {
+    label: "Đăng xuất",
+  },
+]);
+</script>
 <template>
-    <div class="bg-blue-100 flex align-items-center" style="gap: 25rem;">
-    <div class="flex align-items-center gap-8">
-        <div class="w-20rem ml-8">
-            <img src="../public/imagins/Logo-Minh-Thu.png" class="w-full">
-        </div>
-
-        <div class="p-input-icon-left ml-5 w-25rem align-items-center" style="height: 2.5rem;">
-            <i class="pi pi-search -mt-2" />
-            <InputText v-model="value1" placeholder="Search" class="pl-5 w-full h-full"/>
-        </div>
+  <div
+    class="bg-blue-100 flex align-items-center justify-content-between px-8 py-2"
+  >
+    <!-- Logo -->
+    <div class="w-10rem lg:w-15rem">
+      <img src="../public/imagins/Logo-Minh-Thu.png" class="w-full" />
     </div>
-    <div class="flex" style="gap: 10rem;">
-    <div class="flex align-items-center gap-3">
+
+    <!-- Search -->
+    <div
+      class="p-input-icon-left ml-5 w-25rem align-items-center"
+      style="height: 2.5rem"
+    >
+      <i class="pi pi-search -mt-2" />
+      <InputText
+        v-model="value1"
+        placeholder="Search"
+        class="pl-5 w-full h-full"
+      />
+    </div>
+
+    <div class="flex gap-5">
+      <!-- Social -->
+      <div class="flex align-items-center gap-3">
         <div id="FB" class="relative">
-        <i id="fb" class="pi pi-facebook text-3xl"></i>
-            <div id="ifb">
-            <a href="" class="absolute surface-900 text-white flex border-round w-10rem h-2rem align-items-center justify-content-center">Follow on Facebook</a>
-            </div>
-        </div>
-        <div id="INS" class="relative">
-        <i id="ins" class="pi pi-instagram text-3xl"></i>
-            <div id="iins">
-            <a href="" class="absolute surface-900 text-white flex border-round w-10rem h-2rem align-items-center justify-content-center">Follow on Instagram</a>
-            </div>
-        </div>
-        <div id="YT" class="relative">
-        <i id="yt" class="pi pi-youtube text-3xl"></i>
-            <div id="iyt">
-            <a href="" class="absolute surface-900 text-white flex border-round w-10rem h-2rem align-items-center justify-content-center">Follow on Youtube</a>
-            </div>
+          <i id="fb" class="pi pi-facebook text-3xl"></i>
+          <div id="ifb">
+            <a
+              href=""
+              class="absolute surface-900 text-white flex border-round w-10rem h-2rem align-items-center justify-content-center"
+            >
+              Follow on Facebook
+            </a>
+          </div>
         </div>
 
+        <div id="INS" class="relative">
+          <i id="ins" class="pi pi-instagram text-3xl"></i>
+          <div id="iins">
+            <a
+              href=""
+              class="absolute surface-900 text-white flex border-round w-10rem h-2rem align-items-center justify-content-center"
+            >
+              Follow on Instagram
+            </a>
+          </div>
         </div>
-        <div class="flex align-items-center">
-            <div style="width: 2.5rem;">
-                <img src="../public/imagins/hour.png" alt="" class="w-full border-circle">
-            </div>
-            <span class="text-2xl">Username</span>
+
+        <div id="YT" class="relative">
+          <i id="yt" class="pi pi-youtube text-3xl"></i>
+          <div id="iyt">
+            <a
+              href=""
+              class="absolute surface-900 text-white flex border-round w-10rem h-2rem align-items-center justify-content-center"
+            >
+              Follow on Youtube
+            </a>
+          </div>
         </div>
+      </div>
+
+      <!-- UserName after login -->
+      <div
+        class="relative"
+        @mouseover="cancelHideTooltip"
+        @mouseleave="hideTooltip"
+      >
+        <div class="flex align-items-center gap-2">
+          <div
+            style="width: 2.5rem; object-fit: cover"
+            class="-mb-1 border-circle flex align-items-center"
+          >
+            <img
+              src="../public/imagins/hour.png"
+              class="w-full h-full border-circle"
+            />
+          </div>
+
+          <span
+            @mouseover="showTooltip = true"
+            @mouseleave="hideTooltip"
+            class="text-base lg:text-xl cursor-pointer"
+          >
+            Username
+          </span>
+        </div>
+
+        <!-- Tooltip content -->
+        <div
+          v-if="showTooltip"
+          @mouseover="cancelHideTooltip"
+          class="absolute z-5 bg-white py-2 px-3 w-12rem border-round-sm bg-red-200"
+        >
+          <div v-for="(menu, index) in menuUser" :key="index">
+            <span
+              class="cursor-pointer hover:text-blue-700 text-base lg:text-lg"
+              >{{ menu.label }}</span
+            >
+            <hr v-if="index !== menuUser.length - 1" class="my-2" />
+          </div>
+        </div>
+      </div>
     </div>
-</div>
+  </div>
 </template>
+
 <style scoped>
-#ifb{
-    display: none;
+#ifb {
+  display: none;
 }
-#FB:hover #ifb{
-    display: inline;
+#FB:hover #ifb {
+  display: inline;
 }
-#iins{
-    display: none;
+#iins {
+  display: none;
 }
-#INS:hover #iins{
-    display: inline;
+#INS:hover #iins {
+  display: inline;
 }
-#iyt{
-    display: none;
+#iyt {
+  display: none;
 }
-#YT:hover #iyt{
-    display: inline;
+#YT:hover #iyt {
+  display: inline;
 }
 </style>
