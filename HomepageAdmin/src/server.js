3fdf8531f21f1ceb0b60dce9fd5c056e32952c62
Tan Binh <dtbinh2210@gmail.com>
2024-01-04 20:28:22 +0700

    server.js added appointment,dentist and service

--- HomepageAdmin/src/server.js
+++ HomepageAdmin/src/server.js
@@ -5,24 +5,22 @@ const moment = require('moment');
 const cors = require('cors');
 const nodemailer = require('nodemailer');
 const { google } = require('googleapis');
-const bodyParser = require('body-parser');
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 const port = 3000;
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 const OAuth2 = google.auth.OAuth2;
 const oauth2Client = new OAuth2(
-  'Your Client ID',
-  'Your Client Secret',
+  '24544262210-fd1qv4jqnhrnfriijjp25lc2rl9kprgp.apps.googleusercontent.com',
+  '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',
   'https://developers.google.com/oauthplayground'
 );
 
 oauth2Client.setCredentials({
-  refresh_token: 'Your Refresh Token'
+  refresh_token: '1//04RlBTFULbEobCgYIARAAGAQSNwF-L9IrX76khIzigOK_UENkm0y5RTZlcHHXel-qf06htkTGhQesyfyyVwah60Vh75jJXcKsz6A'
 });
 
 const accessToken = oauth2Client.getAccessToken();
@@ -32,9 +30,9 @@ const transporter = nodemailer.createTransport({
   auth: {
       type: 'OAUTH2',
       user: 'binh2210test@gmail.com',
-      clientId: 'Your Client ID',
-      clientSecret: 'Your Client Secret',
-      refreshToken: 'Your Refresh Token',
+      clientId: '24544262210-fd1qv4jqnhrnfriijjp25lc2rl9kprgp.apps.googleusercontent.com',
+      clientSecret: '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',
+      refreshToken: '1//04RlBTFULbEobCgYIARAAGAQSNwF-L9IrX76khIzigOK_UENkm0y5RTZlcHHXel-qf06htkTGhQesyfyyVwah60Vh75jJXcKsz6A',
       accessToken: accessToken,
   }
 });
@@ -68,6 +66,19 @@ app.get('/api/appointments/get/:NGAYKHAM', (req, res) => {
         return row;
       });
       res.json(formattedResults);
+    }
+  });
+});
+app.get('/api/appointments/get/:NGAYKHAM/:MANS', (req, res) => {
+  const query = 'SELECT KHUNGGIO FROM appointment where NGAYKHAM = ? and MANS = ?';
+  const { NGAYKHAM, MANS } = req.params;
+  db.query(query, [NGAYKHAM, MANS] ,(err, results) => {
+    if (err) {
+      console.error('Error executing query:', err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    } else {
+      console.log('SQL query successful');
+      res.json(results);
     }
   });
 });
@@ -186,6 +197,152 @@ app.post('/api/appointments', (req, res) => {
         });  
     });
     res.json('Success');
+});
+
+
+app.get('/api/allnhasi', (req, res) => {
+  const query = 'SELECT * FROM nhasi';
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error('Error executing query:', err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    } else {
+      console.log('Results:', results);
+      res.json(results);
+    }
+  });
+});
+// Định nghĩa API để thêm nha sĩ
+app.post('/api/allnhasi', (req, res) => {
+  const { MANS, TENNS, GIOITHIEU, SDT, DIACHI } = req.body;
+  const sql = 'INSERT INTO nhasi (MANS, TENNS, GIOITHIEU, SDT, DIACHI) VALUES (?, ?, ?, ?, ?)';
+  const values = [MANS, TENNS, GIOITHIEU, SDT, DIACHI];
+  db.query(sql, values, (err, result) => {
+    if (err) {
+      console.error('Error inserting into MySQL:', err);
+      res.status(500).send('Internal Server Error');
+    } else {
+      console.log('Inserted new record:', result);
+      res.status(200).send('Inserted new record');
+    }
+  });
+});
+//sua nha si
+app.put('/api/allnhasi/:MANS', (req, res) => {
+  // Trích xuất dữ liệu từ phần thân của yêu cầu
+  const { TENNS, GIOITHIEU, SDT, DIACHI } = req.body;
+
+  // Trích xuất MADV từ các tham số đường dẫn
+  const MANS = req.params.MANS;
+  // Truy vấn SQL để cập nhật bản ghi trong bảng 'service'
+  const sql = 'UPDATE nhasi SET TENNS = ?, GIOITHIEU = ?, SDT = ? , DIACHI = ? WHERE MANS = ?';
+  // Giá trị để cập nhật trong truy vấn SQL
+  const values = [TENNS, GIOITHIEU, SDT, DIACHI, MANS];
+  // Thực thi truy vấn sử dụng kết nối cơ sở dữ liệu
+  db.query(sql, values, (err, result) => {
+    if (err) {
+      // Xử lý lỗi cơ sở dữ liệu
+      console.error('Lỗi khi cập nhật bản ghi MySQL:', err);
+      res.status(500).send('Lỗi Nội Server');
+    } else {
+      // Ghi log thành công và gửi phản hồi
+      console.log('Bản ghi đã được cập nhật:', result);
+      res.status(200).send('Bản ghi đã được cập nhật');
+    }
+  });
+});
+// delete nha si
+app.delete('/api/allnhasi/:MANS', (req, res) => {
+  // Trích xuất MADV từ các tham số đường dẫn
+  const MANS = req.params.MANS;
+
+  // SQL query để xóa một bản ghi từ bảng 'service'
+  const sql = 'DELETE FROM nhasi WHERE MANS = ?';
+
+  // Thực thi truy vấn sử dụng kết nối cơ sở dữ liệu
+  db.query(sql, [MANS], (err, result) => {
+    if (err) {
+      // Xử lý lỗi cơ sở dữ liệu
+      console.error('Lỗi khi xóa bản ghi MySQL:', err);
+      res.status(500).send('Lỗi Nội Server');
+    } else {
+      // Log thành công và chuyển hướng người dùng sau khi xóa
+      console.log('Bản ghi đã được xóa:', result);
+    }
+  });
+});
+
+// Dịch vụ
+app.get('/api/alldichvu', (req, res) => {
+  const query = 'SELECT * FROM service';
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error('Error executing query:', err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    } else {
+      console.log('Results:', results);
+      res.json(results);
+    }
+  });
+});
+// Thêm dịch vụ
+app.post('/api/alldichvu', (req, res) => {
+  const { MADV, TENDV, GIA, MOTA } = req.body;
+  const sql = 'INSERT INTO service (MADV, TENDV, GIA, MOTA) VALUES (?, ?, ?, ?)';
+  const values = [MADV, TENDV, GIA, MOTA];
+  db.query(sql, values, (err, result) => {
+    if (err) {
+      console.error('Error inserting into MySQL:', err);
+      res.status(500).send('Internal Server Error');
+    } else {
+      console.log('Inserted new record:', result);
+      res.status(200).send('Inserted new record');
+    }
+  });
+});
+
+// update dich vu
+app.put('/api/alldichvu/:MADV', (req, res) => {
+  // Trích xuất dữ liệu từ phần thân của yêu cầu
+  const { TENDV, GIA, MOTA } = req.body;
+
+  // Trích xuất MADV từ các tham số đường dẫn
+  const MADV = req.params.MADV;
+  // Truy vấn SQL để cập nhật bản ghi trong bảng 'service'
+  const sql = 'UPDATE service SET TENDV = ?, GIA = ?, MOTA = ? WHERE MADV = ?';
+  // Giá trị để cập nhật trong truy vấn SQL
+  const values = [TENDV, GIA, MOTA, MADV];
+  // Thực thi truy vấn sử dụng kết nối cơ sở dữ liệu
+  db.query(sql, values, (err, result) => {
+    if (err) {
+      // Xử lý lỗi cơ sở dữ liệu
+      console.error('Lỗi khi cập nhật bản ghi MySQL:', err);
+      res.status(500).send('Lỗi Nội Server');
+    } else {
+      // Ghi log thành công và gửi phản hồi
+      console.log('Bản ghi đã được cập nhật:', result);
+      res.status(200).send('Bản ghi đã được cập nhật');
+    }
+  });
+});
+// delete dịch vụ
+app.delete('/api/alldichvu/:MADV', (req, res) => {
+  // Trích xuất MADV từ các tham số đường dẫn
+  const MADV = req.params.MADV;
+  // SQL query để xóa một bản ghi từ bảng 'service'
+  const sql = 'DELETE FROM service WHERE MADV = ?';
+  // Thực thi truy vấn sử dụng kết nối cơ sở dữ liệu
+  db.query(sql, [MADV], (err, result) => {
+    if (err) {
+      // Xử lý lỗi cơ sở dữ liệu
+      console.error('Lỗi khi xóa bản ghi MySQL:', err);
+      res.status(500).send('Lỗi Nội Server');
+    } else {
+      // Log thành công và chuyển hướng người dùng sau khi xóa
+      console.log('Bản ghi đã được xóa:', result);
+      res.redirect('/danh-sach-dich-vu'); // Điều hướng đến một trang cụ thể
+    }
+  });
 });
 
 app.listen(port, () => {
