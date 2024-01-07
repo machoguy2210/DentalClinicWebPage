f75e4a2779153232b771cf6cdf45ff140631e1f8
Tan Binh <dtbinh2210@gmail.com>
2024-01-07 11:06:44 +0700

    server.js chuẩn

--- HomepageAdmin/src/server.js
+++ HomepageAdmin/src/server.js
@@ -16,13 +16,13 @@ const port = 3000;
 
 const OAuth2 = google.auth.OAuth2;
 const oauth2Client = new OAuth2(
-  'YOUR_CLIENT_ID',
-  'YOUR_CLIENT_SECRET',
+  '24544262210-fd1qv4jqnhrnfriijjp25lc2rl9kprgp.apps.googleusercontent.com',
+  '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',
   'https://developers.google.com/oauthplayground'
 );
 
 oauth2Client.setCredentials({
-  refresh_token: 'YOUR_REFRESH_TOKEN'
+  refresh_token: '1//04RlBTFULbEobCgYIARAAGAQSNwF-L9IrX76khIzigOK_UENkm0y5RTZlcHHXel-qf06htkTGhQesyfyyVwah60Vh75jJXcKsz6A'
 });
 
 const accessToken = oauth2Client.getAccessToken();
@@ -32,9 +32,9 @@ const transporter = nodemailer.createTransport({
   auth: {
       type: 'OAUTH2',
       user: 'binh2210test@gmail.com',
-      clientId: 'YOUR_CLIENT_ID',
-      clientSecret: 'YOUR_CLIENT_SECRET',
-      refreshToken: 'YOUR_REFRESH_TOKEN',
+      clientId: '24544262210-fd1qv4jqnhrnfriijjp25lc2rl9kprgp.apps.googleusercontent.com',
+      clientSecret: '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',
+      refreshToken: '1//04RlBTFULbEobCgYIARAAGAQSNwF-L9IrX76khIzigOK_UENkm0y5RTZlcHHXel-qf06htkTGhQesyfyyVwah60Vh75jJXcKsz6A',
       accessToken: accessToken,
   }
 });
@@ -325,6 +325,327 @@ app.delete('/api/alldichvu/:MADV', (req, res) => {
     }
   });
 });
+
+// * API endpoint để lấy thông tin cuộc hẹn
+app.get('/appointments/:MAKH', (req, res) => {
+  const MAKH = req.params.MAKH;
+
+  const query = `
+    SELECT *
+    FROM appointment
+    INNER JOIN khachhang ON appointment.MAKH = khachhang.MAKH
+    INNER JOIN nhasi ON appointment.MANS = nhasi.MANS
+    INNER JOIN service ON appointment.MADV = service.MADV
+    WHERE appointment.MAKH = ?`;
+
+  db.query(query, [MAKH], (err, results) => {
+    if (err) {
+      console.error('Lỗi truy vấn:', err);
+      res.status(500).json({ error: 'Lỗi truy vấn cơ sở dữ liệu' });
+    } else {
+      res.status(200).json(results);
+    }
+  });
+});
+
+app.delete('/appointments/:MAKH/:NGAYKhAM', (req, res) => {
+  const customerId = parseInt(req.params.MAKH);
+  const appointmentDate = req.params.NGAYKhAM;
+
+  // Kiểm tra xem các tham số bắt buộc đã được cung cấp chưa
+  if (!customerId || !appointmentDate) {
+    return res.status(400).json({ error: 'Missing required parameters' });
+  }
+
+  // Xóa lịch hẹn dựa trên MAKH và NGAYKhAM
+  const query = 'DELETE FROM appointment WHERE MAKH = ? AND NGAYKhAM = ?;';
+
+  db.query(query, [customerId, appointmentDate], (err, result) => {
+    if (err) {
+      console.error('Lỗi khi xóa lịch hẹn:', err);
+      res.status(500).json({ error: 'Lỗi khi xóa lịch hẹn' });
+    } else {
+      if (result.affectedRows === 0) {
+        res.status(404).json({ error: 'Không tìm thấy lịch hẹn để xóa' });
+      } else {
+        res.status(200).json({ message: `Đã xóa lịch hẹn cho khách hàng có ID ${customerId} vào ngày ${appointmentDate}` });
+      }
+    }
+  });
+});
+
+// * API chỉnh sửa thông tin cuộc hẹn
+app.put('/appointments/:MAKH/:NGAYKhAM', (req, res) => {
+  const customerId = parseInt(req.params.MAKH);
+  const appointmentDate = req.params.NGAYKhAM;
+
+  // Kiểm tra xem req.body có tồn tại không và có chứa các trường cần thiết không
+  if (!req.body || !req.body.KHUNGGIO || !req.body.GHICHU) {
+    return res.status(400).json({ error: 'Thiếu thông tin cần thiết trong body request' });
+  }
+
+  const { KHUNGGIO, GHICHU } = req.body;
+
+  // Kiểm tra xem các tham số bắt buộc đã được cung cấp chưa
+  if (!KHUNGGIO || !GHICHU) {
+    return res.status(400).json({ error: 'Thiếu tham số bắt buộc' });
+  }
+
+  // Cập nhật thông tin lịch khám trong cơ sở dữ liệu
+  const query = `
+    UPDATE appointment
+    SET KHUNGGIO = ?, GHICHU = ?
+    WHERE MAKH = ? AND NGAYKhAM = ?;
+  `;
+
+  db.query(query, [KHUNGGIO, GHICHU, customerId, appointmentDate], (err, result) => {
+    if (err) {
+      console.error('Lỗi khi cập nhật thông tin lịch khám:', err);
+      return res.status(500).json({ error: 'Lỗi khi cập nhật thông tin lịch khám' });
+    }
+
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ error: `Không tìm thấy lịch khám cho khách hàng có ID ${customerId} vào ngày ${appointmentDate}` });
+    }
+
+    res.status(200).json({ message: `Đã cập nhật lịch khám cho khách hàng có ID ${customerId} vào ngày ${appointmentDate}` });
+  });
+});
+
+// API endpoint để lấy toàn bộ thông tin nhân viên
+app.get('/employees', (req, res) => {
+  const query = 'SELECT * FROM nhanvien;';
+
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error('Lỗi truy vấn:', err);
+      res.status(500).json({ error: 'Lỗi truy vấn cơ sở dữ liệu' });
+    } else {
+      res.status(200).json(results);
+    }
+  });
+});
+
+// API endpoint để thêm nhân viên mới
+app.post('/employees', (req, res) => {
+  const { HOTEN, EMAIL, SDT, GIOITINH, DIACHI, NGAYSINH, PASSWORD } = req.body;
+
+  // Kiểm tra xem các tham số bắt buộc đã được cung cấp chưa
+  if (!HOTEN || !EMAIL || !SDT || !GIOITINH || !DIACHI || !NGAYSINH || !PASSWORD) {
+    return res.status(400).json({ error: 'Thiếu tham số bắt buộc' });
+  }
+
+  // Thêm nhân viên mới vào cơ sở dữ liệu
+  const query = `
+      INSERT INTO nhanvien (HOTEN, EMAIL, SDT, GIOITINH, DIACHI, NGAYSINH, PASSWORD)
+      VALUES (?, ?, ?, ?, ?, ?, ?);
+    `;
+
+  db.query(query, [HOTEN, EMAIL, SDT, GIOITINH, DIACHI, NGAYSINH, PASSWORD], (err, result) => {
+    if (err) {
+      console.error('Lỗi khi thêm nhân viên mới:', err);
+      return res.status(500).json({ error: 'Lỗi khi thêm nhân viên mới' });
+    }
+
+    res.status(201).json({ message: 'Đã thêm nhân viên mới thành công' });
+  });
+});
+
+// API endpoint để xóa nhân viên dựa trên ID
+app.delete('/employees/:id', (req, res) => {
+  const employeeId = parseInt(req.params.id);
+
+  // Xóa nhân viên có ID tương ứng
+  const query = 'DELETE FROM nhanvien WHERE MANV = ?;';
+
+  db.query(query, [employeeId], (err, result) => {
+    if (err) {
+      console.error('Lỗi khi xóa nhân viên:', err);
+      res.status(500).json({ error: 'Lỗi khi xóa nhân viên' });
+    } else {
+      res.status(200).json({ message: `Đã xóa nhân viên có ID ${employeeId}` });
+    }
+  });
+});
+
+// API endpoint để chỉnh sửa thông tin nhân viên
+app.put('/employees/:id', (req, res) => {
+  const employeeId = parseInt(req.params.id);
+
+  // Kiểm tra xem req.body có tồn tại và có chứa các trường cần thiết không
+  const { HOTEN, EMAIL, SDT, GIOITINH, DIACHI, NGAYSINH, PASSWORD } = req.body;
+  if (!HOTEN || !EMAIL || !SDT || !GIOITINH || !DIACHI || !NGAYSINH || !PASSWORD) {
+    return res.status(400).json({ error: 'Thiếu thông tin cần thiết trong body request' });
+  }
+
+  // Cập nhật thông tin nhân viên trong cơ sở dữ liệu
+  const query = `
+    UPDATE nhanvien
+    SET HOTEN = ?, EMAIL = ?, SDT = ?, GIOITINH = ?, DIACHI = ?, NGAYSINH = ?, PASSWORD = ?
+    WHERE MANV = ?;
+  `;
+
+  db.query(query, [HOTEN, EMAIL, SDT, GIOITINH, DIACHI, NGAYSINH, PASSWORD, employeeId], (err, result) => {
+    if (err) {
+      console.error('Lỗi khi cập nhật thông tin nhân viên:', err);
+      return res.status(500).json({ error: 'Lỗi khi cập nhật thông tin nhân viên' });
+    }
+
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ error: `Không tìm thấy nhân viên có ID ${employeeId}` });
+    }
+
+    res.status(200).json({ message: `Đã cập nhật nhân viên có ID ${employeeId}` });
+  });
+});
+
+// API endpoint để thêm mới nhân viên
+app.post('/employees', (req, res) => {
+  // Kiểm tra xem req.body có tồn tại và có chứa các trường cần thiết không
+  const { HOTEN, EMAIL, SDT, GIOITINH, DIACHI, NGAYSINH, PASSWORD } = req.body;
+  if (!HOTEN || !EMAIL || !SDT || !GIOITINH || !DIACHI || !NGAYSINH || !PASSWORD) {
+    return res.status(400).json({ error: 'Thiếu thông tin cần thiết trong body request' });
+  }
+
+  // Thêm mới nhân viên vào cơ sở dữ liệu
+  const query = `
+    INSERT INTO nhanvien (HOTEN, EMAIL, SDT, GIOITINH, DIACHI, NGAYSINH, PASSWORD)
+    VALUES (?, ?, ?, ?, ?, ?, ?);
+  `;
+
+  db.query(query, [HOTEN, EMAIL, SDT, GIOITINH, DIACHI, NGAYSINH, PASSWORD], (err, result) => {
+    if (err) {
+      console.error('Lỗi khi thêm mới nhân viên:', err);
+      return res.status(500).json({ error: 'Lỗi khi thêm mới nhân viên' });
+    }
+
+    res.status(201).json({ message: 'Đã thêm mới nhân viên thành công' });
+  });
+});
+
+// API endpoint để xóa nhân viên dựa trên ID
+app.delete('/customers/:id', (req, res) => {
+  const customerId = parseInt(req.params.id);
+
+  // Xóa nhân viên có ID tương ứng
+
+  const query = 'DELETE FROM khachhang WHERE MAKH = ?;';
+
+  db.query(query, [customerId], (err, result) => {
+    if (err) {
+      console.error('Lỗi khi xóa nhân viên:', err);
+      res.status(500).json({ error: 'Lỗi khi xóa nhân viên' });
+    } else {
+      res.status(200).json({ message: `Đã xóa nhân viên có ID ${customerId}` });
+    }
+  });
+});
+
+app.get('/customers', (req, res) => {
+  const query = 'SELECT * FROM khachhang;';
+  db.query(query, (err, results) => {
+    res.status(200).json(results);
+  })
+});
+
+app.get('/customers/:name', (req, res) => {
+  const name1 = parseInt(req.params.name);
+
+  const query = 'SELECT * FROM khachhang WHERE HOTEN LIKE=?';
+  db.query(query, (err, results) => {
+    res.status(200).json(results);
+  })
+});
+
+app.put('/customers/:id', (req, res) => {
+  const customerId = parseInt(req.params.id);
+
+  // Kiểm tra xem req.body có tồn tại và có chứa các trường cần thiết không
+  const { HOTEN, EMAIL, SDT, GIOITINH, DIACHI, NGAYSINH, PASSWORD, TICHDIEM } = req.body;
+  if (!HOTEN || !EMAIL || !SDT || !GIOITINH || !DIACHI || !NGAYSINH || !TICHDIEM || !PASSWORD) {
+    return res.status(400).json({ error: 'Thiếu thông tin cần thiết trong body request' });
+  }
+
+  // Cập nhật thông tin nhân viên trong cơ sở dữ liệu
+  const query = `
+    UPDATE khachhang
+    SET HOTEN = ?, EMAIL = ?, PASSWORD = ?, SDT = ?, GIOITINH = ?, DIACHI = ?, NGAYSINH = ?,TICHDIEM=?
+    WHERE MAKH = ?;
+  `;
+
+  db.query(query, [HOTEN, EMAIL, PASSWORD, SDT, GIOITINH, DIACHI, NGAYSINH, TICHDIEM, customerId], (err, result) => {
+    if (err) {
+      console.error('Lỗi khi cập nhật thông tin nhân viên:', err);
+      return res.status(500).json({ error: 'Lỗi khi cập nhật thông tin nhân viên' });
+    }
+
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ error: `Không tìm thấy nhân viên có ID ${customerId}` });
+    }
+
+    res.status(200).json({ message: `Đã cập nhật nhân viên có ID ${customerId}` });
+  });
+});
+
+app.get('/uudai', (req, res) => {
+  const query = 'SELECT * FROM uudai ORDER BY TICHDIEM DESC;';
+
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error('Lỗi truy vấn:', err);
+      res.status(500).json({ error: 'Lỗi truy vấn cơ sở dữ liệu' });
+    } else {
+      res.status(200).json(results);
+    }
+  });
+});
+
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    const query = 'Select * from KHACHHANG where EMAIL = ? and PASSWORD = ?';
+    db.query(query, [email, password], (err0, results0) => {
+        if (err0) {
+            console.error('Error executing query:', err);
+            res.status(500).json({ error: 'Internal Server Error' });
+        } else {
+          if (results0.length > 0) {
+            results0[0].CATEGORY = 0;
+            res.status(200).json(results0);
+          } 
+          else {
+            const query1 = 'Select * from NHANVIEN where EMAIL = ? and PASSWORD = ?';
+            db.query(query1, [email, password], (err1, results1) => {
+              if (err1) {
+                console.error('Error executing query:', err1);
+                res.status(500).json({ error: 'Internal Server Error' });
+              } else {
+                if (results1.length > 0) {
+                  results1[0].CATEGORY = 1;
+                  res.status(200).json(results1);
+                } else {
+                  const query2 = 'Select * from ADMIN where EMAIL = ? and PASSWORD = ?';
+                  db.query(query2, [email, password], (err2, results2) => {
+                    if (err2) {
+                      console.error('Error executing query:', err2);
+                      res.status(500).json({ error: 'Internal Server Error' });
+                    } else {
+                      if (results2.length > 0) {
+                        results2[0].CATEGORY = 2;
+                        res.status(200).json(results2);
+                      } else {
+                        res.status(404).json("No user found");
+                      }
+                    }
+                  });
+                }
+              }
+            })
+          }
+        }
+    })
+});
+
+            
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
