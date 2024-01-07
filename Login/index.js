529139e2822ae378d818a6d512371a6b29801628
Tan Binh <dtbinh2210@gmail.com>
2024-01-07 11:14:31 +0700

    login fix

--- Login/index.js
+++ Login/index.js
@@ -1,4 +1,19 @@
 
-    function Redirect() {
-        window.location.href = "../dist/index.html"
+    document.getElementById("login").onclick = () => {
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+        if (email === "" || password === "") {
+            alert("Please fill all the fields");
+            return;
+        }
+        else
+        axios.post(`http://localhost:3000/login`, { email, password })
+            .then(res => {
+                if (res.status == 200) {
+                    if (res.data[0].CATEGORY == 0) {
+                        window.location.href = "";
+                        localStorage.setItem("user", JSON.stringify(res.data[0].MAKH, res.data[0].HOTEN, res.data[0].EMAIL, res.data[0].SDT, res.data[0].DIACHI)); 
+                    }
+                }
+            })
     }
